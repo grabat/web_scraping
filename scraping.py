4c09f61a79dd1515e7df91f4e2c9f1038e4bcd2b
kazuyuki ikeda <gankai1104@gmail.com>
2018-04-21 00:17:03 +0900

    Refactor scraping.py

--- scraping.py
+++ scraping.py
@@ -3,35 +3,75 @@ import lxml.html
 import psycopg2
 import setttings.py
 
-conn = psycopg2.connect(
-    dbname=os.environ.get("DATABASE_NAME"),
-    user=os.environ.get("DATABASE_USER"),
-    password=os.environ.get("DATABASE_PASSWORD"),
-    host=os.environ.get("DATABASE_HOST"),
-    port=os.environ.get("DATABASE_PORT")
-    )
-# S3からファイルをダウンロードかイベントを検知して、ファイルを読み込む
-file_name = 'result.html'
 
-raw_data = open('result.html', 'r').read()
-doc = lxml.html.fromstring(raw_data)
-service_name = 'shuumatsu-worker'
-expire_date = ''
-url = os.environ.get("COOPERATIVE_SERVICE")
-path = file_name
+class ActiveRecord:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            dbname=os.environ.get("DATABASE_NAME"),
+            user=os.environ.get("DATABASE_USER"),
+            password=os.environ.get("DATABASE_PASSWORD"),
+            host=os.environ.get("DATABASE_HOST"),
+            port=os.environ.get("DATABASE_PORT")
+        )
 
-price = doc.xpath("/html/body/section/div[2]/div/div[2]/div/div[5]/dl[2]/dd").text()
-title = doc.xpath("/html/body/section/div[2]/div/div[2]/div/div[2]/h2").text()
+    def save(self):
+        pass
 
-cursor = conn.cursor()
-records = cursor.execute("SELECT * FROM crawler_data WHERE id=%s", FILE_NAME)
+    def close(self):
+        pass
 
-if records.fetchone():
-  # 更新
-  cursor.execute("UPDATE crawlers SET raw_data = %s, title = %s, price = %s WHERE id = %s", raw, title, price, id)
-else:
-  # 新
-  cursor.execute("INSERT INTO crawlers (raw_data, title, price, path, url, service_name, expire_date) VALUES (%s, %s, %s, %s, %s, %s, %s)", (raw_data, title, price, path, url, service_name, expire_date))
 
-# conn.commit()
-conn.close()
+class CrawlerData(ActiveRecord):
+    def update(self):
+        cursor.execute(
+            "UPDATE crawlers "
+            "SET raw_data = %s, title = %s, price = %s WHERE id = %s",
+            raw_data,
+            title,
+            price,
+            id)
+
+    def save(self):
+        cursor.execute(
+            "INSERT INTO crawlers ("
+            "raw_data, title, price, path, url, service_name, expire_date"
+            ") VALUES ("
+            "%s, %s, %s, %s, %s, %s, %s"
+            ")",
+            (raw_data,
+             title,
+             price,
+             path,
+             url,
+             service_name,
+             expire_date))
+
+    def close(self):
+        self.conn.close
+
+
+class Scraping:
+    cralwer_data = CrawlerData()
+    # S3からファイルをダウンロードかイベントを検知して、ファイルを読み込む
+    file_name = 'result.html'
+    raw_data = open(file_name, 'r').read()
+    doc = lxml.html.fromstring(raw_data)
+    service_name = os.environ.get("COOPERATIVE_SERVICE")
+    url = os.environ.get("COOPERATIVE_SERVICE_URL")
+    expire_date = ''
+    path = file_name
+
+    price = doc.xpath(
+        "/html/body/section/div[2]/div/div[2]/div/div[5]/dl[2]/dd").text()
+    title = doc.xpath(
+        "/html/body/section/div[2]/div/div[2]/div/div[2]/h2").text()
+
+    cursor = active_record.conn.cursor()
+    records = cursor.execute(
+        "SELECT * FROM crawler_data WHERE id=%s",
+        file_name)
+
+    if records.fetchone():
+        cralwer_data.update()
+    else:
+        cralwer_data.create()
